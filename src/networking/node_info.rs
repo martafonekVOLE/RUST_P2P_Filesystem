6e3b8dcd12a113bf12ebb2a24396e9eb0c2246fb
VileDeg <gvadim134@gmail.com>
2025-01-12 00:17:45 +0100

    feat: Add unit tests for k-bucket, fix r-table unit tests

--- src/networking/node_info.rs
+++ src/networking/node_info.rs
@@ -1,8 +1,9 @@
 use crate::core::key::Key;
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::net::{SocketAddr, Ipv4Addr};
+use rand::Rng;
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
 pub struct NodeInfo {
     pub id: Key,
     pub address: SocketAddr,
@@ -23,10 +24,15 @@ impl NodeInfo {
 
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
-
         bytes.extend(self.id.to_bytes());
         bytes.extend(self.address.to_string().as_bytes());
-
         bytes
+    }
+
+    pub fn create_local_node() -> Self {
+        let id = Key::new_random();
+        let socket = std::net::UdpSocket::bind("127.0.0.1:0").expect("Failed to bind to address");
+        let address = socket.local_addr().expect("Failed to get local address");
+        NodeInfo { id, address }
     }
 }
--- src/routing/routing_table.rs
+++ src/routing/routing_table.rs
@@ -7,36 +7,77 @@ use tokio::sync::RwLock;
 
 pub struct RoutingTable {
     id: Key,
-    buckets: Vec<Arc<RwLock<KBucket>>>,
-    bucket_size: usize,
+    //buckets: Vec<Arc<RwLock<KBucket>>>,
+    buckets: Vec<KBucket>,
+    bucket_size: usize, // TODO: remove?
 }
 
 impl RoutingTable {
-    // TODO should this be 160?
-    pub const NUM_BUCKETS: usize = 160;
-    
+    // Must be 160
+    pub const NUM_BUCKETS: usize = K * 8; // Key byte length * bits_per_byte
+
     pub fn new(id: Key) -> Self {
-        let buckets = (0..Self::NUM_BUCKETS)
-            .map(|_| Arc::new(RwLock::new(KBucket::new())))
-            .collect();
+        // let buckets = (0..Self::NUM_BUCKETS)
+        //     .map(|_| Arc::new(RwLock::new(KBucket::new())))
+        //     .collect();
         RoutingTable {
             id,
-            buckets,
+            buckets: vec![KBucket::new(); Self::NUM_BUCKETS],
             bucket_size: K,
         }
     }
 
-    pub async fn store_nodeinfo(&self, node_info: NodeInfo) -> bool {
-        let bucket_index = self.id.leading_zeros(&node_info.get_id()) as usize;
-        if let Some(bucket) = self.buckets.get(bucket_index) {
-            let mut bucket = bucket.write().await;
+    pub fn store_nodeinfo(&mut self, node_info: NodeInfo) -> bool {
+        let bucket_index = self.id.leading_zeros(&node_info.get_id());
+        if let Some(bucket) = self.buckets.get_mut(bucket_index) {
             bucket.add_node(node_info)
         } else {
             false
         }
     }
 
-    pub fn get_buckets(&self) -> Vec<Arc<RwLock<KBucket>>> {
-        self.buckets.clone()
+    // pub fn get_buckets(&self) -> Vec<Arc<RwLock<KBucket>>> {
+    //     self.buckets.clone()
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+    use crate::core::key::Key;
+    use crate::networking::node_info::NodeInfo;
+    use crate::routing::routing_table::RoutingTable;
+
+    #[test]
+    fn test_initialize() {
+        let id = Key::new_random();
+        let routing_table = RoutingTable::new(id);
+
+        // Verify the number of buckets
+        assert_eq!(routing_table.buckets.len(), RoutingTable::NUM_BUCKETS);
+        // for bucket in &routing_table.get_buckets() {
+        //     assert!(bucket.read()); // Ensure each bucket is initialized
+        // }
+    }
+    #[test]
+    fn test_store_nodeinfo_success() {
+        let id = Key::from_input(b"local_node");
+        let mut routing_table = RoutingTable::new(id);
+
+        let remote_id = Key::from_input(b"remote_node");
+        let remote_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 1, 1)), 8080);
+        let node_info = NodeInfo::new(remote_id, remote_address);
+
+        // Store the node and verify success
+        let result = routing_table.store_nodeinfo(node_info.clone());
+        assert!(result);
+
+        // Verify the node is added to the correct bucket
+        let bucket_index = id.leading_zeros(&remote_id);
+        let bucket = routing_table.buckets.get(bucket_index).unwrap();
+        //let bucket = bucket.read().await;
+
+        assert!(bucket.find_node(&node_info.get_id()).is_some());
     }
 }
