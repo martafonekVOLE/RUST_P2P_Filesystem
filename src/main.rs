e48b36cfaabd15c5c9275c79e104a8f83850788b
VileDeg <gvadim134@gmail.com>
2025-01-12 14:24:00 +0100

    style(main): Format, remove redundant mod, add doc

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-mod cli;
-mod config;
-mod core;
-mod networking;
-mod routing;
-mod sharding;
-mod storage;
-mod utils;
-
-use clap::{Command, Arg};
+use clap::{Arg, Command};
 use std::error::Error;
 
 #[derive(Debug)]
@@ -22,30 +13,47 @@ impl Arguments {
             .version("1.0.0")
             .author("TODO")
             .about("TODO")
-            .arg(
-                Arg::new("config")
-                    .short('c')
-                    .long("config")
-            )
-            .arg(
-                Arg::new("port")
-                    .short('p')
-                    .long("port")
-            )
+            .arg(Arg::new("config").short('c').long("config"))
+            .arg(Arg::new("port").short('p').long("port"))
             .get_matches();
 
         Ok(Self {
-            config: matches.get_one::<String>("config").expect("Config is required").clone(),
-            port: matches.get_one::<String>("port").unwrap().parse::<u16>().expect("Port is required").clone(),
+            config: matches
+                .get_one::<String>("config")
+                .expect("Config is required")
+                .clone(),
+            port: matches
+                .get_one::<String>("port")
+                .unwrap()
+                .parse::<u16>()
+                .expect("Port is required")
+                .clone(),
         })
     }
 }
 
 fn main() {
     let config = Arguments::new().expect("Failed to parse CLI arguments");
-    
+
     println!("{:?}", config);
 
     println!("Config file path: {}", config.config);
     println!("Port: {}", config.port);
 }
+
+/*
+
+* - required
+^ - TRUE by default
+
+^   -c --config: Config file with node's configuration. In YAML format.
+        Contains:
+            - beacon_node address: ip+port
+            - node's port
+            - cache file path: where to dump node's config when it turns off
+            - storage path: where to store donwloaded files
+    -b --beacon: Is it a beacond node? If so, beacon node's address is ignored if encountered in config file.
+    --cache: Use the cache logic. Dump config to cache file when turned off.
+    -v --verbose: Log info about the ongoing communication to stdout. (For debugging purposes)
+    -h --help: Print help message
+ */
